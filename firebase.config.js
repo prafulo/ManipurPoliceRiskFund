991887dd743e0e9bc51650ba2592eaa310a655f6
Prafulo (Globizs) <prafulo@globizs.com>
2026-01-06 11:09:03 +0000

    Try fixing this error: `Runtime FirebaseError: Firebase: Error (auth/inv

---
+++ firebase.config.js
@@ -0,0 +1,16 @@
+// This file is used to configure the Firebase project.
+// It is used by the Firebase CLI and by the app.
+
+// To get this config, go to the Firebase console, select your project,
+// and then go to Project settings > General.
+
+const firebaseConfig = {
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+};
+
+export default firebaseConfig;
--- src/firebase/index.ts
+++ src/firebase/index.ts
@@ -2,29 +2,21 @@
 import { initializeApp, getApp, getApps, type FirebaseOptions } from 'firebase/app';
 import { getAuth, connectAuthEmulator } from 'firebase/auth';
 import { getFirestore, connectFirestoreEmulator } from 'firebase/firestore';
+import firebaseConfig from '../../firebase.config.js';
 
 // Re-export hooks and providers
 export * from './provider';
 export * from './hooks/use-collection';
 export * from './hooks/use-doc';
 export * from './hooks/use-auth-user';
-
-
-const firebaseConfig: FirebaseOptions = {
-    apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
-    authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
-    projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
-    storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
-    messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
-    appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
-};
+export * from './client-provider';
 
 
 function initializeFirebase() {
     if (getApps().length > 0) {
         return getApp();
     }
-    return initializeApp(firebaseConfig);
+    return initializeApp(firebaseConfig as FirebaseOptions);
 }
 
 export const firebaseApp = initializeFirebase();
