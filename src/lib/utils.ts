92360a03d85e12a7a90ad1f4f13dbd15db0143d1
Prafulo (Globizs) <prafulo@globizs.com>
2026-01-06 11:31:43 +0000

    try again

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -7,6 +7,7 @@ export function cn(...inputs: ClassValue[]) {
 
 // Function to convert number to words
 export function numberToWords(num: number): string {
+    if (num === 0) return 'Zero';
     const a = [
         '', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine', 'ten', 'eleven', 'twelve', 'thirteen', 'fourteen', 'fifteen', 'sixteen', 'seventeen', 'eighteen', 'nineteen'
     ];
@@ -14,11 +15,12 @@ export function numberToWords(num: number): string {
         '', '', 'twenty', 'thirty', 'forty', 'fifty', 'sixty', 'seventy', 'eighty', 'ninety'
     ];
     
-    if (num === 0) return 'zero';
+    const numStr = num.toString();
+    if (numStr.length > 9) return 'overflow';
 
-    if ((num = num.toString()).length > 9) return 'overflow';
-    const n = ('000000000' + num).substr(-9).match(/^(\d{2})(\d{2})(\d{2})(\d{1})(\d{2})$/);
+    const n = ('000000000' + numStr).substr(-9).match(/^(\d{2})(\d{2})(\d{2})(\d{1})(\d{2})$/);
     if (!n) return '';
+
     let str = '';
     str += (n[1] != '00') ? (a[Number(n[1])] || b[Number(n[1][0])] + ' ' + a[Number(n[1][1])]) + ' crore ' : '';
     str += (n[2] != '00') ? (a[Number(n[2])] || b[Number(n[2][0])] + ' ' + a[Number(n[2][1])]) + ' lakh ' : '';
@@ -26,5 +28,6 @@ export function numberToWords(num: number): string {
     str += (n[4] != '0') ? (a[Number(n[4])] || b[Number(n[4][0])] + ' ' + a[Number(n[4][1])]) + ' hundred ' : '';
     str += (n[5] != '00') ? ((str != '') ? 'and ' : '') + (a[Number(n[5])] || b[Number(n[5][0])] + ' ' + a[Number(n[5][1])]) : '';
 
-    return str.trim().split(/\s+/).map(s => s.charAt(0).toUpperCase() + s.substring(1)).join(' ');
+    const finalStr = str.trim().split(/\s+/).map(s => s.charAt(0).toUpperCase() + s.substring(1)).join(' ');
+    return finalStr;
 }
